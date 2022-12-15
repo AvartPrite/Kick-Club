03aac453c8c4e6de2cac75482e476607c1e239ab
AvartPrite <avartprite@yandex.ru>
2022-12-15 16:18:32 +0300

    minor changes

--- Program.cs
+++ Program.cs
@@ -1,37 +1,35 @@
 ﻿using System.Threading;
+
 class Battle{
 
     static int Randomizer(int x, int y){
         Random rnd = new();
         return rnd.Next(x, y);
     }
-    static int DiceRoll(){
-        Random dice = new();
-        return dice.Next(0, 21);
-    }
-
     static void Statistic(int heroHp, int enemyHp){
         Console.WriteLine($"Здоровье Героя: {heroHp}\nЗдоровье Врага: {enemyHp}\n");
     }
     
     static void Main(){
+
         int round = 1;
+        
         Fighter Hero = new Fighter(Randomizer(100, 121),Randomizer(10, 16));
         Fighter Enemy = new Fighter(Randomizer(100, 121),Randomizer(10, 16));
 
         Statistic(Hero.HP, Enemy.HP);
-        
+
         Console.WriteLine("Fight!\n");
         
         while (true){
             Console.WriteLine($"Раунд: {round++}\n");
 
-            if (DiceRoll() > 17){
+            if (Randomizer(0, 21) > 17){
                 Enemy.HP -= Hero.Str * 2;
                 Console.WriteLine($"Крит!\nГерой наносит: {Hero.Str * 2} урона\n");
                 Statistic(Hero.HP, Enemy.HP);
             }
-            else if (DiceRoll() < 6){
+            else if (Randomizer(0, 21) < 6){
                 Console.WriteLine("Герой промахнулся!\n");
             }
             else{
@@ -46,12 +44,12 @@ class Battle{
 
             Thread.Sleep(1500);
             
-            if (DiceRoll() > 17){
+            if (Randomizer(0, 21) > 17){
                 Hero.HP -= Enemy.Str * 2;
                 Console.WriteLine($"Крит!\nВраг наносит: {Enemy.Str * 2} урона\n");
                 Statistic(Hero.HP, Enemy.HP);
             }
-            else if (DiceRoll() < 6){
+            else if (Randomizer(0, 21) < 6){
                 Console.WriteLine("Враг промахнулся!\n");
             }
             else{
